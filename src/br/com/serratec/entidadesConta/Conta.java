09ca2bd0c4ca90121199a6a026a0602aca6be2a7
Gabriel Fernandes <97479003+gaabezk@users.noreply.github.com>
2022-04-27 15:40:55 -0300

    Updated Conta

--- src/br/com/serratec/entidadesConta/Conta.java
+++ src/br/com/serratec/entidadesConta/Conta.java
@@ -2,8 +2,40 @@ package br.com.serratec.entidadesConta;
 
 public abstract class Conta {
 
+	protected String cpfTitular;
+	protected double saldo;
 	
 	
+	public Conta(String cpfTitular) {
+		if(cpfTitular.length() == 11) {
+			this.cpfTitular = cpfTitular;
+		}
+	}
 	
+	public String getCpfTitular() {
+		return cpfTitular;
+	}
+
+	public double getSaldo() {
+		return saldo;
+	}
 	
+	public void deposito(double quantidade) {
+		this.saldo += quantidade;
+	}
+	public void saque(double quantidade) {
+		if(this.saldo > quantidade) {
+			this.saldo -= quantidade;
+		}
+		
+	}
+	
+	@Override
+	public String toString() {
+		return
+				 "\nCPFtitular: " 
+				+ cpfTitular 
+				+ "\nsaldo: " 
+				+ saldo;
+	}
 }
