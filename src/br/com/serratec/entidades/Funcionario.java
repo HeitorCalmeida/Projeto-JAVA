5903a826f623c3c4a87ffd0da0f2a10cba52de54
HeitorCalmeida <heitorcalmeida@gmail.com>
2022-04-27 15:08:07 -0300

    update funcionario

--- src/br/com/serratec/entidades/Funcionario.java
+++ src/br/com/serratec/entidades/Funcionario.java
@@ -1,8 +1,19 @@
 package br.com.serratec.entidades;
 
 public class Funcionario extends Usuario{
+	private String cargo;
 	
-	
-	
-	
+	public Funcionario(String cpf, String nome, String senha) {
+		super(cpf, nome, senha);
+		this.cargo = cargo;
+	}
+
+	public String getCargo() {
+		return cargo;
+	}
+
+	public void setCargo(String cargo) {
+		this.cargo = cargo;
+	}
+
 }
