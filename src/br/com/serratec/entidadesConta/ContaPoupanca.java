a04eb7ebbbae20a420eff88c9c4a77d1dce4386f
HeitorCalmeida <heitorcalmeida@gmail.com>
2022-04-28 14:48:34 -0300

    update conta poupança

--- src/br/com/serratec/entidadesConta/ContaPoupanca.java
+++ src/br/com/serratec/entidadesConta/ContaPoupanca.java
@@ -1,5 +1,18 @@
 package br.com.serratec.entidadesConta;
 
-public class ContaPoupanca {
+public class ContaPoupanca extends Conta{
+		private double rendimento;
+
+		public ContaPoupanca(String cpfTitular, double rendimento) {
+			super(cpfTitular);
+			this.rendimento = rendimento;
+			this.tipoConta = "Conta-Poupança";
+		}
+
+		public double getRendimento() {
+			return rendimento;
+		}
+		
+		
 
 }
